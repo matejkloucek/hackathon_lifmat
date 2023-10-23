04bd83c9eb1f92c1bb1391bfcf908d2077669e6b
abragtim <timur.abragimovich@mild.blue>
2023-10-23 23:12:27 +0200

    Add run in docker.

--- backend/app.py
+++ backend/app.py
@@ -36,6 +36,7 @@ def add_namespaces(api: Api):
     api.add_namespace(hello_api)
 
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     app.run(host='localhost', port=8080, debug=True)
